5221591822fbf1fc735e59944b611e5bb8596026
jeff-k <jeff@x-0.ca>
2018-04-27 11:29:52 -0700

    Revert "Extract string building from rustbio to avoid doing it twice."
    This reverts commit 03c730c495a3783b0049e19cecb60676f2e5f937.

--- pyrustbio/__init__.py
+++ pyrustbio/__init__.py
@@ -1,15 +1,13 @@
 from . import _pyrustbio
 
-
 def pairwise(query, ref, g_o, g_e):
-    score, query_a, ref_a = _pyrustbio.affine(query, ref, g_o, g_e)
-    return score, query_a, ref_a
-
+    score, pretty = _pyrustbio.affine(query, ref, g_o, g_e)
+    return score, pretty.split('\n')[0:3]
 
 def levenshtein(a, b):
     return _pyrustbio.levenshtein(a, b)
 
-
 def align_it(ref, query, g_o, g_e, terminal_p):
-    score, query_a, ref_a = _pyrustbio.affine(query, ref, -1 * g_o, -1 * g_e)
-    return ref_a, query_a, score
+    score, pretty = _pyrustbio.affine(query, ref, -1 * g_o, -1 * g_e)
+    ref_a, _, query_a = pretty.split('\n')[0:3]
+    return ref_a.replace(' ', '-'), query_a.replace(' ', '-'), score
